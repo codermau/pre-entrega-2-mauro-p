76a3964742cc47b2bb0635bac17eb28f44651674
codermau <106565258+codermau@users.noreply.github.com>
2022-10-30 21:58:13 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Cart" element={<Cart />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
